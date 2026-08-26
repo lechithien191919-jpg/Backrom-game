2887044d026471c683b5bcb1914f62baa4ea95bb
lechithien191919-jpg <lechithien191919@gmail.com>
2026-08-26 21:02:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,53 +1,20 @@
 from flask import Flask, render_template, request, jsonify
-import os
-import psycopg2
 
 app = Flask(__name__)
-
-DATABASE_URL = os.environ.get("DATABASE_URL")
-
-def get_db_connection():
-    conn = psycopg2.connect(DATABASE_URL)
-    return conn
-
-def init_db():
-    try:
-        conn = get_db_connection()
-        cur = conn.cursor()
-        cur.execute('''
-            CREATE TABLE IF NOT EXISTS players (
-                id SERIAL PRIMARY KEY,
-                username VARCHAR(50),
-                score INT
-            )
-        ''')
-        conn.commit()
-        cur.close()
-        conn.close()
-    except Exception as e:
-        print("DB Init Error:", e)
-
-init_db()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# API tạm thời khi chưa có database
 @app.route('/save_score', methods=['POST'])
 def save_score():
     data = request.json
     username = data.get('username')
     score = data.get('score')
-    
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('INSERT INTO players (username, score) VALUES (%s, %s)', (username, score))
-    conn.commit()
-    cur.close()
-    conn.close()
-    
-    return jsonify({"status": "success", "message": "Đã lưu dữ liệu lên Neon DB!"})
+    print(f"Nhận được điểm từ {username}: {score}s")
+    return jsonify({"status": "success", "message": "Đã nhận điểm (chưa lưu db)!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
-  
+    
