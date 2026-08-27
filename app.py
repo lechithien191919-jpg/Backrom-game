4d294b34b012231485f10c067e02c7c758088a76
lechithien191919-jpg <lechithien191919@gmail.com>
2026-08-27 13:46:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
-from flask import Flask, render_template, request, jsonify
-
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='.') # Cho phép đọc file html ngay tại thư mục hiện tại luôn
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
-
-# API tạm thời khi chưa có database
-@app.route('/save_score', methods=['POST'])
-def save_score():
-    data = request.json
-    username = data.get('username')
-    score = data.get('score')
-    print(f"Nhận được điểm từ {username}: {score}s")
-    return jsonify({"status": "success", "message": "Đã nhận điểm (chưa lưu db)!"})
-
-if __name__ == '__main__':
-    app.run(debug=True)
     
